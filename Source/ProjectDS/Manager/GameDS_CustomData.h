64620ed9600074f054a71b573426d8baa0ea7f05
DESKTOP-HLU2P7C\dmlru <ug4306@gmail.com>
2025-08-31 01:20:05 +0900

    1. CustomData 작성 2. DataManager 구조 작성

--- Source/ProjectDS/Manager/GameDS_CustomData.h
+++ Source/ProjectDS/Manager/GameDS_CustomData.h
@@ -14,6 +14,14 @@ USTRUCT(BlueprintType)
 struct FGameDS_SpawnData
 {
 	GENERATED_BODY()
+	UPROPERTY(EditAnywhere)
+	EGameDS_UnitType UnitType;
+
+	UPROPERTY(EditAnywhere)
+	EGameDS_HeroClassType HeroClassType;
+
+	UPROPERTY(EditAnywhere)
+	int32 DataID = INDEX_NONE;
 
 	UPROPERTY(EditAnywhere, meta = (DisplayThumbnail = true))
 	TSubclassOf<ACharacter> CharacterBP;
@@ -27,8 +35,370 @@ class UGameDS_SpawnUnitData : public UDataAsset
 
 public:
 	UPROPERTY(EditAnywhere)
-	EGameDS_UnitType UnitType;
-
-	UPROPERTY(EditAnywhere)
 	TArray<FGameDS_SpawnData> SpawnDataList;
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_HeroStatConfigDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameDS_HeroStatInfo HeroStatInfo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameDS_HeroCreateInfo HeroCreateInfo;
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_EnemyStatConfigDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameDS_UnitStatInfo UnitStatInfo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PhysicalAttackValue = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MagicalAttackValue = 0.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float WalkSpeed = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SprintSpeed = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float FaceOffSpeed = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PatrolSpeed = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaximumChaseableDistance = 2500.0f;
+
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_SkillDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SkillName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(DisplayThumbnail=true))
+	UAnimMontage* SkillMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_SkillType SkillType = EGameDS_SkillType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CoolTime = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CriticalPercentage = 0.0f;
+
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_SkillInfoDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SkillInfoName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 CollisionInfoID = INDEX_NONE;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<int32> CrowdControlInfoIDList;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DamageValue = 0.0f;
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_CollisionInfoDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 CollisionInfoID = INDEX_NONE;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_CollisionShape CollisionShape = EGameDS_CollisionShape::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_SkillSpawnType CollisionSpawnType = EGameDS_SkillSpawnType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Param0 = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Param1 = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Param2 = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Param3 = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Param4 = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Param5 = 0.0f;
+
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_EnemySkillType : uint8
+{
+	None = 0,
+	Normal,
+	ForwardSpecial,
+	BackwardSpecial
+	FaceOff,
+	Num,
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_EnemySkillSettingDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_EnemySkillType SkillType = EGameDS_EnemySkillType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 WaitCount = INDEX_NONE;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinDistance = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxDistance = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinAngleDifference = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float RotateSpeedFactor = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MoveBlockDuration = 0.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float AttackBlockDuration = 0.0f;
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_ItemType : uint8
+{
+	None = 0,
+	Potion,
+	Weapon
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_ItemDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_ItemType ItemType = EGameDS_ItemType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName ItemName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh* ItemMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* ItemIcon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString ItemDescription;
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_SkillSetDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SkillSetName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<int32> SkillSet;
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_SummonType : uint8
+{
+	None = 0,
+	Default,
+	Follower
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_SummonDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_SummonType SummonType = EGameDS_SummonType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SummonName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_SkillSpawnType SpawnType = EGameDS_SkillSpawnType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 SkillInfoID = INDEX_NONE;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 SummonSpeed = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SummonDuration = INDEX_NONE;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(DisplayThumbnail=true))
+	TSubclassOf<AGameDS_Summon> SummonBP = nullptr;
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_GripType : uint8
+{
+	None,
+	OneHand,
+	TwoHand
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_WeaponType : uint8
+{
+	None,
+	Axe,
+	Sword,
+	Shield,
+	LongSword
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_WeaponSpawnType : uint8
+{
+	None,
+	LeftWeapon,
+	RightWeapon,
+	LeftShield,
+	RightShield
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_WeaponDataTable : public FGameDS_ItemDataTable
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 WeaponID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_GripType GripType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_WeaponType WeaponType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_WeaponSpawnType SpawnType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(DisplayThumbnail=true))
+	TSubclassOf<UGameDS_UnitAnimInstance> UnitAnim = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayThumbnail = true))
+	TMap<FString,UAnimMontage*> Montages;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RequiredVaitality = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RequiredEndurance = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RequiredStrength = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RequiredIntelligence = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PhysicalDamage = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MagicDamage = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PhysicalGuard = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MagicGuard = 0.0f;
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_PotionDataTable : public FGameDS_ItemDataTable
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 DefaultUseCount = 0;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DefaultHealValue = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CoolTime = 5.0f;
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_CrowdControlType : uint8
+{
+	None = 0,
+	PushBack,
+	Stun,
+	Num
+};
+
+UENUM(BlueprintType)
+enum class EGameDS_CrowdControlMoveType : uint8
+{
+	None = 0,
+	CasterToTarget,
+	TargetToCaster,
+	SummonToTarget,
+	SummonToCaster,
+	Num
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_CrowdControlData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_CrowdControlType CrowdControlType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameDS_CrowdControlMoveType CrowdControlMoveType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Distance = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MoveBlockDuration = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ActionBlockDuration = 0.0f;
+};
+
+USTRUCT(BlueprintType)
+struct FGameDS_CrowdControlInfoDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName CrowdControlName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameDS_CrowdControlData CrowdControlData;
 };
--- Source/ProjectDS/Manager/GameDS_DataManager.h
+++ Source/ProjectDS/Manager/GameDS_DataManager.h
@@ -20,12 +20,202 @@ class PROJECTDS_API UGameDS_DataManager : public UGameInstanceSubsystem
 public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
-	const FGameDS_SpawnData* GetSpawnData();
-	const FTransform GetSpawnPointTransform(int32 PointNum);
-private:
 
+	template <typename DataStruct>
+	const UDataTable* GetDataTable() const;
+	template <typename DataStruct>
+	const DataStruct GetDataInRow(int32 DataID);
+
+	//Data Getter
+	const FGameDS_SpawnData* GetSpawnData(); const;
+	const UGameDS_HeroStatConfigData* GetHeroStatConfigData(); const;
+	void GetHeroStatConfigData(FGameDS_UnitStatInfo& UnitStatInfo, FGameDS_HeroStatInfo& HeroStatInfo);
+	void GetHeroCreateData(FGameDS_HeroCreateInfo& HeroCreateInfo);
+	void GetEnemyStatConfigData(int32 InDataID, FGameDS_UnitStatInfo& UnitStatInfo);
+	const FGameDS_SkillDataTable* GetSkillData(int32 InSkillID) const;
+	const FGameDS_SkillInfoDataTable* GetSkillInfoData(int32 InSkillID) const;
+	const FGameDS_CollisionInfoDataTable* GetCollisionInfoData(int32 InCollisionID) const;
+	FGameDS_WeaponDataTable* GetWeaponData(int32 InWeaponID) const;
+
+	void InitLastSerialID(int32 InLastSerialID);
+	int32 CreateItemSerialID();
+
+	UFUNCTION(BlueprintCallable)
+	void LoadHeroData(FGameDS_HeroCreateInfo& CreateInfo);
+
+	UFUNCTION(BlueprintCallable)
+	void SaveHeroData(FGameDS_HeroCreateInfo& InHeroCreateInfo, bool bDefaultSetting = false);
+	
+	UFUNCTION(BlueprintCallable)
+	void SaveDataWithBool(FString Section, FString Key, const bool Value);
+	UFUNCTION(BlueprintCallable)
+	void SaveDataWithInt(FString Section, FString Key, const int32 Value);
+	UFUNCTION(BlueprintCallable)
+	void SaveData(FString Section, FString Key, const TArray<FString>& Value);
+	
+	UFUNCTION(BlueprintCallable)
+	bool LoadData(FString Section, FString Key, TArray<FString>& Value);
+	UFUNCTION(BlueprintCallable)
+	bool LoadDataWithBool(FString Section, FString Key, bool& Value);
+	UFUNCTION(BlueprintCallable)
+	bool LoadDataWithInt(FString Section, FString Key, int32& Value);
+
+	UFUNCTION(BlueprintCallable)
+	void SaveFirstData(FGameDS_HeroCreateInfo InHeroCreateInfo);
+
+	UFUNCTION(BlueprintCallable)
+	FTransform GetSpawnPointTransform();
+
+	UFUNCTION(BlueprintCallable)
+	bool IsValidSpawnPoint(int32 InSpawnPoint);
+
+	UFUNCTION(BlueprintCallable)
+	bool IsValidGameData() const;
+
+private:
+	template <typename DataStruct>
+	void AddDataTable(UDataTable* DataTable);
+
+	void InitSkillData();
+	void InitSkillInfoData();
+
+	void InitItemData();
+
+	// DataAsset
 	UPROPERTY(Config)
 	FSoftClassPath SpawnUnitDataPath;
 	UPROPERTY()
 	UGameDS_SpawnUnitData* SpawnUnitData;
+
+	UPROPERTY(Config)
+	FSoftClassPath HeroStatConfigDataPath;
+	UPROPERTY()
+	UGameDS_HeroStatConfigData* HeroStatConfigData;
+
+	UPROPERTY(Config)
+	FSoftClassPath DefaultHeroSettingDataPath;
+	UPROPERTY()
+	UGameDS_DefaultHeroSetting* DefaultHeroSetting;
+
+	// DataTable
+	UPROPERTY(Config)
+	FSoftClassPath EnemyStatConfigDataPath;
+	UPROPERTY()
+	UDataTable* EnemyStatConfigData;
+	
+	UPROPERTY(Config)
+	FSoftClassPath HeroSkillDataPath;
+	UPROPERTY()
+	UDataTable* HeroSkillData;
+	
+	UPROPERTY(Config)
+	FSoftClassPath EnemySkillDataPath;
+	UPROPERTY()
+	UDataTable* EnemySkillData;
+
+	UPROPERTY(Config)
+	FSoftClassPath HeroSkillInfoDataPath;
+	UPROPERTY()
+	UDataTable* HeroSkillInfoData;
+
+	UPROPERTY(Config)
+	FSoftClassPath EnemySkillInfoDataPath;
+	UPROPERTY()
+	UDataTable* EnemySkillInfoData;
+
+	// common
+	UPROPERTY()
+	UDataTable* SkillData;
+	
+	UPROPERTY()
+	UDataTable* SkillInfoData;
+	//
+
+	UPROPERTY(Config)
+	FSoftClassPath CollisionInfoDataPath;
+	UPROPERTY()
+	UDataTable* CollisionInfoData;
+	
+	UPROPERTY(Config)
+	FSoftClassPath EnemySkillSettingDataPath;
+	UPROPERTY()
+	UDataTable* EnemySkillSettingData;
+
+	// common
+	UPROPERTY()
+	UDataTable* ItemData;
+	//
+
+	UPROPERTY(Config)
+	FSoftClassPath SkillSetPath;
+	UPROPERTY()
+	UDataTable* SkillSetData;
+
+	UPROPERTY(Config)
+	FSoftClassPath HeroSkillSetPath;
+	UPROPERTY()
+	UDataTable* HeroSkillSetData;
+
+
+	UPROPERTY(Config)
+	FSoftClassPath SummonDataPath;
+	UPROPERTY()
+	UDataTable* SummonData;
+
+
+	UPROPERTY(Config)
+	FSoftClassPath WeaponDataPath;
+	UPROPERTY()
+	UDataTable* WeaponData;
+
+	UPROPERTY(Config)
+	FSoftClassPath PotionDataPath;
+	UPROPERTY()
+	UDataTable* PotionData;
+
+	UPROPERTY(Config)
+	FSoftClassPath CrowdControlInfoDataPath;
+	UPROPERTY()
+	UDataTable* CrowdControlInfoData;
+
+	UPROPERTY(Config)
+	FSoftClassPath StatusEffectInfoDataPath;
+	UPROPERTY()
+	UDataTable* StatusEffectInfoData;
+
+	int32 LastItemSerialID = INDEX_NONE;
+	TArray<int32> ItemSerialIDList;
+
+	UPROPERTY()
+	TMap<FName, UDataTable*> DataTableMap;
+
+	UPROPERTY()
+	FGameDS_HeroCreateInfo HeroCreateInfo;
 };
+
+template <typename DataStruct>
+const UDataTable* UGameDS_DataManager::GetDataTable()
+{
+	auto FindData = DataTableMap.Find(DataStruct::StaticStruct()->GetFName());
+	return FindData ? *FindData : nullptr;
+}
+
+template <typename DataStruct>
+const DataStruct* UGameDS_DataManager::GetDataInRow(int32 DataID)
+{
+	const UDataTable* FindData = GetDataTable<DataStruct>();
+	if (FindData)
+	{
+		return FindData->FindRow<DataStruct>(FName(*FString::Printf(TEXT("%d"), DataID)), TEXT(""));
+	}
+	return nullptr;
+}
+
+template <typename DataStruct>
+void UGameDS_DataManager::AddDataTable(UDataTable* DataTable)
+{
+	if(DataTable == nullptr)
+		return;
+
+	DataTableMap.Add(DataStruct::StaticStruct()->GetFName(), DataTable);
+}
